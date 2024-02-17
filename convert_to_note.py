30af5dbf173ffd096be7628a2868ff015bcc6238
Osman Yucel <mail@osmanyucel.com>
2024-02-16 21:28:13 -0800

    Update convert_to_note.py

--- convert_to_note.py
+++ convert_to_note.py
@@ -4,11 +4,11 @@ from calibre.gui2.tweak_book.boss import get_boss
 
 
 def create_reference(note_num):
-    return "<a href=\"notes.htm#n{note_num}\" id=\"n{note_num}d\"><sup>[{note_num}]</sup></a>".format(note_num=note_num)
+    return "<sup><a href=\"notes.htm#n{note_num}\" id=\"n{note_num}d\">[{note_num}]</a></sup>".format(note_num=note_num)
 
 
 def create_footnote(file_name, note_num, content):
-    return "<p><a href=\"{file_name}#n{note_num}d\" id=\"n{note_num}\">{note_num}</a> {content}</p>".format(
+    return "<div><a href=\"{file_name}#n{note_num}d\" id=\"n{note_num}\">{note_num}</a><p> {content}</p></div>".format(
         file_name=file_name, note_num=note_num, content=content)
 
 
@@ -41,13 +41,13 @@ def create_notes_file_if_missing(title):
 </body>
 </html>
         """.format(title)
-    current_container().add_file("notes.htm", html.encode("UTF-16"), media_type="text/html")
+    current_container().add_file("notes.htm", html.encode("UTF-16"), media_type="application/xhtml+xml")
     
 def replace(match, number, file_name, metadata, dictionaries, data, functions, *args, **kwargs):
     get_boss().commit_all_editors_to_container()
     create_notes_file_if_missing(metadata.title)
     note_num = find_next_note_num()
-    append_note(create_footnote(file_name, note_num, match.group()))
+    append_note(create_footnote(file_name, note_num, match.group(1)))
     get_boss().apply_container_update_to_gui()
     
 
